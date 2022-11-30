1c98c38bd37607b003e21fdbb50066aa71043134
Erin Hurlburt <77364422+erinhurlburt@users.noreply.github.com>
2022-11-30 20:28:13 +0000

    Add files via upload

---
+++ Model/Article.swift
@@ -0,0 +1,19 @@
+//
+//  Article.swift
+//  Babel Beauty
+//
+//  Created by Sebastian Cruz on 4/30/22.
+//
+
+/**
+ * This defines the struct used to represent an individual article in the blog.
+ */
+import Foundation
+
+struct Article: Hashable, Codable, Identifiable {
+    var id: String
+    var title: String
+    var date: Date
+    var body: String
+}
+
